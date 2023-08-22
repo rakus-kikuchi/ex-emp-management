c0d3851c5cbc5540e7f6fdf9f3f646d7bc44cbb8
Rinko Kikuchi <rinko.kikuchi@rakus-partners.co.jp>
2023-08-22 17:54:38 +0900

    loginFormクラス作成

--- src/main/java/com/example/form/LoginForm.java
+++ src/main/java/com/example/form/LoginForm.java
@@ -2,5 +2,23 @@ package com.example.form;
 
 //ログイン時
 public class LoginForm {
+    private String mailAddress;
+    private String password;
+    @Override
+    public String toString() {
+        return "LoginForm [mailAddress=" + mailAddress + ", password=" + password + "]";
+    }
+    public String getMailAddress() {
+        return mailAddress;
+    }
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
 }
