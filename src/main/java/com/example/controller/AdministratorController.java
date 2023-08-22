87132f94bee8c50336c219991d7fe1edd1874562
Rinko Kikuchi <rinko.kikuchi@rakus-partners.co.jp>
2023-08-22 16:22:34 +0900

    insertメソッド作成

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 
@@ -21,4 +24,14 @@ public class AdministratorController {
         return "administrator/insert";
     }
     
-}
+
+    @PostMapping("/insert")
+    private String insert(InsertAdministratorForm form){
+        Administrator administrator = new Administrator();
+        //formの中身をadministratorにコピー
+        BeanUtils.copyProperties(form, administrator);
+        //insertメソッドを呼び出し
+        administratorService.insert(administrator);
+        return "redirect:/";
+    }
+    }
